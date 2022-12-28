87f44411bbe5ed335c77c91236d31e2bd43cf761
Joan Killeen <joan.killeen@bluewin.ch>
2022-12-28 10:49:19 +0100

    modified:   cypress/e2e/automation-test-store/contact-us.js
    Using CSS selectors

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,17 +1,32 @@
 /// <reference types="Cypress" />
 
-describe("Test Contact Us Form via Automation Test Store", () =>{
-    it("Should be able to open succcessfullly contact us form", ()=>{
+describe("Test Contact Us Form via Automation Test Store using CSS selectors", () =>{
+    it("Should be able to open successfullly contact us form using CSS selectors only", ()=>{
        cy.visit("https://www.automationteststore.com/")
-       cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click()
-       cy.get('#ContactUsFrm_first_name').type("Joan")
+       //Find url that ends with 'content/contact'
+       cy.get('a[href$="rt=content/contact"]').click()
+       cy.get('[id=ContactUsFrm_first_name]').type("Joan")
        cy.get('#ContactUsFrm_email').type("joan.test@test.ch")
        cy.get('#ContactUsFrm_enquiry').type("My test enquiry")
-       cy.get('[title="Submit"]').click()
+       cy.get("form#ContactUsFrm button[type='submit']").click()
 
        cy.url().should('contains', 'success')
 
 
     });
+
+    it.only("Should be able to open successfullly contact us form using XPath selectors only", ()=>{
+        cy.visit("https://www.automationteststore.com/")
+        //Find url that ends with 'content/contact'
+        cy.get('a[href$="rt=content/contact"]').click()
+        cy.get('[id=ContactUsFrm_first_name]').type("Joan")
+        cy.get('#ContactUsFrm_email').type("joan.test@test.ch")
+        cy.get('#ContactUsFrm_enquiry').type("My test enquiry")
+        cy.get("form#ContactUsFrm button[type='submit']").click()
+ 
+        cy.url().should('contains', 'success')
+ 
+ 
+     });
     
 })
