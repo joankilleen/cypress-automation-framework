824f14dcaeba6c2abd510ebb945ce2057c6b2913
Joan Killeen <joan.killeen@bluewin.ch>
2023-01-02 14:45:22 +0100

    Challenge

--- cypress/e2e/automation-test-store/alias-invoke.js
+++ cypress/e2e/automation-test-store/alias-invoke.js
@@ -4,10 +4,18 @@
 describe("Alias and invoke", () => {
 
     const productCategory = "Hair Care"
-     it.only("Validate hair care product", () => {
+     it("Validate hair care product", () => {
          cy.visit("https://www.automationteststore.com/")
          cy.get("a[href*='product/category&path=']").contains(productCategory).click()
          cy.get('.fixed_wrapper .prdocutname').eq(0).invoke('text').as('productThumbnail')
          cy.get('@productThumbnail').its('length').should('be.gt', 5)
+         cy.get('@productThumbnail').should('be.eq', 'Seaweed Conditioner')
      });
+
+     it.only("Challenge - verify products with thumbnails on home page", function(){
+        cy.visit("https://www.automationteststore.com/")
+        cy.get('.thumbnail').as('thumbnail').should('have.length', 16)
+        cy.get('@thumbnail').find('.productcart').invoke('attr', 'title').should('include', 'Add to Cart')
+
+     })
  })
