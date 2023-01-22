8e0da6010aaaa8a4a5d937a06b36ee99a2af4bf5
Joan Killeen <joan.killeen@bluewin.ch>
2023-01-22 16:40:58 +0100

    Checkboxes and drop downs

--- cypress/e2e/webdriver-uni/checkboxes.js
+++ cypress/e2e/webdriver-uni/checkboxes.js
@@ -1,18 +1,32 @@
 /// <reference types="Cypress" />
 
-describe("Test Contact Us Form WebdriverUni", () =>{
-    it("Should be able to open succcessfullly contact us form", ()=>{
+describe("Select checkboxes and check state", () =>{
+    it("Should be able to check and uncheck checkbox and verify state", ()=>{
         cy.visit("http://www.webdriveruniversity.com/")
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
         cy.get('input[value="option-1"]').check()
         cy.get('input[value="option-4"]').check()
-        cy.get('input[value="option-3"]').should('be.checked')
-        cy.get('input[value="option-3"]').uncheck()
+        cy.get('input[value="option-3"]').as('option-3').should('be.checked')
+        cy.get('@option-3').uncheck().should('not.be.checked')
 
-        
-        
-        
-    });
+        //Check multiple values
+        cy.get('#checkboxes input').as('checkboxes').check(['option-2', 'option-3'])
+        cy.get('@checkboxes').uncheck(['option-1', 'option-4'])         
+    });    
+})
 
-    
+describe("Handling radio buttons and checking their state", ()=>{
+    it("Should be  able to check radio button and verify the state", ()=>{
+        cy.visit("http://www.webdriveruniversity.com/")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+        cy.get('#radio-buttons [value="orange"]').as('orange').click()
+        cy.get('@orange').should('be.checked')
+    })
+
+    it("Checking disabled state", ()=>{
+        cy.visit("http://www.webdriveruniversity.com/")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+        cy.get("#radio-buttons-selected-disabled [value='cabbage']").as('cabbage').should('be.disabled')
+        cy.get("#radio-buttons-selected-disabled [value='pumpkin']").as('pumpkin').should('not.be.disabled')
+    })
 })
---
+++ cypress/e2e/webdriver-uni/drop-downs.js
@@ -0,0 +1,15 @@
+// <reference types="Cypress" />
+
+describe("Handling drop-down and selecting options", () =>{
+    it("Should be able to select a menu option and verify selection", ()=>{
+        cy.visit("http://www.webdriveruniversity.com/")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+        cy.get('#dropdowm-menu-1').as('menu-1').select('python')
+        cy.get('#dropdowm-menu-2').as('menu-2').select('maven')
+        cy.get('#dropdowm-menu-3').as('menu-3').select('jquery')
+        cy.get('@menu-1').should('have.value', 'python')
+        cy.get('@menu-1').should('not.have.value', 'java')
+        cy.get('@menu-2').should('have.value', 'maven')
+    });    
+})
+
